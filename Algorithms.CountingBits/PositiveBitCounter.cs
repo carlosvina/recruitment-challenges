6113083cc6229c9e57abf320771aeabcf72c6731
carlosvina <712971+carlosvina@users.noreply.github.com>
2020-10-16 14:26:39 +0200

    Implemented PositiveBitCounter

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -5,13 +5,32 @@
 namespace Algorithms.CountingBits
 {
     using System;
+    using System.Linq;
     using System.Collections.Generic;
 
     public class PositiveBitCounter
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0) throw new ArgumentException("Parameter must be a positive integer.");
+            
+            var oneBitCount = 0;  // number of 1's in binary representation
+            int bitPosition = 0;   // bit position being evaluated
+            List<int> positionList = new List<int>();
+            
+            while (input > 0)
+            {
+                if ((input & 1) == 1)
+                {
+                    oneBitCount++;
+                    positionList.Add(bitPosition);
+                }
+
+                bitPosition++;
+                input >>= 1;
+            }
+
+            return positionList.Prepend(oneBitCount);
         }
     }
 }
